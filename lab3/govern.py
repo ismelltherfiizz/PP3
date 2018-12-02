ade73d9b00caa63196bc417b7122329ebc68ba08
Mykhailo Shkolnyi <shkolniy@dlit.dp.ua>
2018-12-02 20:09:50 +0200

    lab 4 deleted

--- lab3/govern.py
+++ lab3/govern.py
@@ -1,35 +1,33 @@
-from collections import defaultdict
-
-
 class Graph:
+    def dfs(self, v):
+        self.untraversed[v] = False
+        for i in self.graph[v]:
+            if i in self.untraversed.keys():
+                self.dfs(i)
+        self.list.append(v)
+
     def __init__(self, graph):
-        self.graph = graph
-        self.traversed = {}
-        self.stack = []
-        for i in graph.keys():
-            if i not in self.traversed:
-                self.dfs(i)
+        self.list = []
+        self.graph = set()
+        untraversed = set(graph.keys())
 
-    def dfs(self, vertex):
-        self.traversed[vertex] = True
-        for i in self.graph[vertex]:
-            if i not in self.traversed.keys():
-                self.dfs(i)
-        self.stack.append(vertex)
+        while untraversed:
+            for i in graph.keys():
+                if i in self.untraversed:
+                    self.dfs(i)
 
 
-if __name__ == "__main__":
-    docs = defaultdict(list)
+def main():
+    docs = []
+    
     for line in open("govern.in"):
         vertexes = line.split()
 
-        if len(vertexes) != 2:
-            break
-
         docs[vertexes[0]].append(vertexes[1])
 
-    result = Graph(docs).stack
-    print(result)
-    govern_out = open("govern.out", 'w')
-    for res in result:
-        govern_out.write(res + "\n")
+    out = Graph(docs).list
+
+    print(out)
+
+if __name__ == '__main__':
+    main()
